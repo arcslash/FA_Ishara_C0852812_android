6beccb2657d1d32984dc1ead53e5c639c2807db4
Ishara Abeykoon <isharaux@gmail.com>
2022-06-18 00:05:14 -0400

    adding splash and shared preferences

---
+++ app/src/main/java/com/lambton/fa_ishara_c0852812_android/SharedPreference.java
@@ -0,0 +1,44 @@
+package com.lambton.fa_ishara_c0852812_android;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+
+
+public class SharedPreference {
+    public static String My_Prefrences = "final_Preferences";
+    static SharedPreferences mPref;
+    Editor editor;
+
+    public SharedPreference(Context mContext) {
+        mPref = mContext.getSharedPreferences(My_Prefrences, Context.MODE_PRIVATE);
+        editor = mPref.edit();
+    }
+
+
+
+
+    public static String getLatitude() {
+        return mPref.getString("LATITUDE", "");
+    }
+
+    public static void setLatitude(String status) {
+        Editor editor2 = mPref.edit();
+        editor2.putString("LATITUDE", status);
+        editor2.apply();
+    }
+
+    public static String getLongitude() {
+        return mPref.getString("LONGITUDE", "");
+    }
+
+    public static void setLongitude(String status) {
+        Editor editor2 = mPref.edit();
+        editor2.putString("LONGITUDE", status);
+        editor2.apply();
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/lambton/fa_ishara_c0852812_android/SplashActivity.java
@@ -0,0 +1,77 @@
+package com.lambton.fa_ishara_c0852812_android;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 1;
+    Location currentLocation;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    private static final int REQUEST_CODE = 101;
+
+    SharedPreference sharedPreference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        sharedPreference=new SharedPreference(SplashActivity.this);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        fetchLocation();
+    }
+
+    private void fetchLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            return;
+        }
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    currentLocation = location;
+                    Toast.makeText(getApplicationContext(), currentLocation.getLatitude() + "" + currentLocation.getLongitude(), Toast.LENGTH_SHORT).show();
+                    new Handler().postDelayed(new Runnable() {
+
+                        @Override
+                        public void run() {
+                        }
+                    }, SPLASH_TIME_OUT);
+
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    fetchLocation();
+                }
+                break;
+        }
+    }
+}
